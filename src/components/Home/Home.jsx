cbd519802b4230f18688dae8fab128cc66f42c4a
Serhii Khabuzov <habuzovs@gmail.com>
2024-01-21 02:01:05 +0100

    fix main

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom"
+import { Container } from "components/Container"
+
 
 export const Home = () => {
     return (
         <>
+        <Container>
         <h1 className="text-white">Home</h1>
         <div className="text-blue-500 font-sans font-medium text-base leading-6">
-            This is a Tailwind CSS component!
+            This is a CAR RENT!
+          <car/> 
+
             </div>
+        </Container>
         </>
     )
 }
